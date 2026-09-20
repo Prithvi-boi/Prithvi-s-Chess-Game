02fb21703e7272b8e6d5b8628a3ac4c9071097dd
Prithvi-boi <prithvi.bel13@gmail.com>
2026-09-21 02:12:53 +0530

    Fixed Responsive Slidebar Issue - Navigation

--- ChessGame/src/Features/Navigation.jsx
+++ ChessGame/src/Features/Navigation.jsx
@@ -10,7 +10,7 @@ import PiecesIcon from "../Assets/Icons/Navigation Icons/Pieces Icon.svg?react"
 import ProfileIcon from "../Assets/Icons/Navigation Icons/Profile Icon.svg?react"
 import RatingsIcon from "../Assets/Icons/Navigation Icons/Ratings Icon.svg?react"
 
-function NavigationButtons({ option, flex_direction, btnStyle, line }) {
+function NavigationButtons({ option, flex_direction, btnStyle, line, Callback = () => { } }) {
     const [Selected, setSelected] = useState(option)
     const btnNames = ['Game', 'Profile', 'Ratings', 'Pieces', 'Board']
     const btns = btnNames.map((name) => {
@@ -24,21 +24,25 @@ function NavigationButtons({ option, flex_direction, btnStyle, line }) {
             case 'Board': Icon = <BoardIcon className={IconStyle} />; break;
             default: break;
         }
+
         return (
-            <button key={name} onClick={(e) => setSelected(e.target.innerText === "" ? name : e.target.innerText)} className={`${name.toLowerCase() == Selected.toLowerCase() ? 'bg-[#009E84] text-white' : ''} flex ${!line ? 'flex-col' : 'flex-row'} justify-center`}>
-                <div className={`flex items-center gap-2 my-auto ${btnStyle} order-2 `}>
+            <button key={name} onClick={(e) => {
+                const selectedName = e.target.innerText === "" ? name : e.target.innerText;
+                Callback(selectedName);
+                setSelected(selectedName);
+            }} className={`${name.toLowerCase() == Selected.toLowerCase() ? 'bg-[#009E84] text-white' : ''} flex xl:flex-col flex-row justify-center`}>
+                <div className={`flex items-center gap-2 my-auto ${btnStyle} order-2`}>
                     <p>{Icon}</p>
                     <p className="font-bold">{name}</p>
                 </div>
-                <div className={`${name.toLowerCase() == Selected.toLowerCase() ? `${!line ? 'h-1 w-full bg-white mt-auto' : 'h-full w-1 bg-white'}` : ''}  ${line ? line : "order-3"}`}></div>
+                <div className={`${name.toLowerCase() == Selected.toLowerCase() ? 'xl:h-1 xl:w-full h-full w-1 bg-white' : ''} order-1 xl:order-3`}></div>
             </button>
         )
     })
-
     return <div className={`flex ${flex_direction} bg-[#262522] text-[#868686]`}> {btns} </div>
 }
 
-export default function Navigation({ option }) {
+export default function Navigation({ option, NavCallback = () => { } }) {
 
     // Navigation for smaller devices
     const [sideBar, setSideBar] = useState(false)
@@ -50,18 +54,15 @@ export default function Navigation({ option }) {
                 <HamburgerIcon />
             </div>
 
-            <div className="h-10  hidden xl:flex">
-                <NavigationButtons option={option} btnStyle={"w-24 justify-center mx-auto"} />
-            </div>
+            <div className={`xl:hidden fixed inset-0 z-10 ${sideBar ? 'bg-black/50' : 'bg-black/0 pointer-events-none'} transition-all`} onClick={() => setSideBar(false)} />
 
-            {/* Mobile or Tablet - SideBar */}
-            <div className={`xl:hidden fixed inset-0 z-10 ${sideBar ? 'bg-black/50' : 'bg-black/0  pointer-events-none'} flex justify-end transition-all`}></div>
-            <div className={`xl:hidden flex flex-col fixed right-0  ${sideBar ? 'translate-x-0 shadow-xl shadow-black' : 'translate-x-full '} inset-y-0 w-40 max-w-[100vw] bg-[#2C2A26] z-20 transition-all ease-in duration-75 gap-10 `}>
-                <div className="ml-auto mx-10 block xl:hidden mt-10" onClick={() => setSideBar(prev => !prev)}>
+            <div className={`fixed ${!sideBar ? '-right-100' : 'right-0'} xl:transition-none gap-7 xl:gap-0 inset-y-0 xl:static xl:h-10 xl:flex ${sideBar ? 'translate-x-0 shadow-xl shadow-black' : 'translate-x-full xl:translate-x-0'}  flex flex-col xl:flex-row w-40 xl:w-auto bg-[#2C2A26] xl:bg-transparent z-20 transition-transform ease-in duration-75`}>
+                <div className="ml-auto mx-10 mt-10 xl:hidden" onClick={() => setSideBar(prev => !prev)}>
                     <CrossIcon />
                 </div>
-                <div className="overflow-scroll w-full flex flex-col" onClick={() => setSideBar(prev => !prev)}>
-                    <NavigationButtons option={option} flex_direction={"flex-col"} btnStyle={"pl-4 h-14 mr-auto"} line={'order-1'} />
+
+                <div className="w-full flex flex-col xl:flex-row">
+                    <NavigationButtons Callback={NavCallback} option={option} flex_direction="flex-col xl:flex-row" btnStyle="w-24 h-14 xl:h-10 pl-4 xl:pl-0 justify-center mx-auto" line="order-1" />
                 </div>
             </div>
         </div>
